650939ab6399246181412ab7ad68eeb6600c8800
AStovpyak <stovpyak@gmail.com>
2018-01-21 14:54:36 +0300

    add exception handling

--- SemaphoreSample/SemaphoreSample/Calc.cs
+++ SemaphoreSample/SemaphoreSample/Calc.cs
@@ -14,15 +14,30 @@ namespace SemaphoreSample
 
         public void Work()
         {
-            Console.WriteLine("start... " + Thread.CurrentThread.Name);
+            try
+            {
+                Console.WriteLine("start... " + Thread.CurrentThread.Name);
 
-            _semaphore.WaitOne();
-            Console.WriteLine("work start " + Thread.CurrentThread.Name);
-            Thread.Sleep(1000);
-            Console.WriteLine("work end " + Thread.CurrentThread.Name);
-            _semaphore.Release();
+                _semaphore.WaitOne();
+                try
+                {
+                    Console.WriteLine("work start " + Thread.CurrentThread.Name);
+                    Thread.Sleep(1000);
+                    int b = 0;
+                    var a = 123 / b;
+                    Console.WriteLine("work end " + Thread.CurrentThread.Name);
+                }
+                finally
+                {
+                    _semaphore.Release();
+                }
 
-            Console.WriteLine("end... " + Thread.CurrentThread.Name);
+                Console.WriteLine("end... " + Thread.CurrentThread.Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
