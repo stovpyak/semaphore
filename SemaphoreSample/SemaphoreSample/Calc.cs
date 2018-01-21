319ba3e9470176db4ef5edbfacb02a7106a6326f
AStovpyak <stovpyak@gmail.com>
2018-01-21 14:30:10 +0300

    first

---
+++ SemaphoreSample/SemaphoreSample/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SemaphoreSample
+{
+    public class Calc
+    {
+        private Semaphore _semaphore;
+
+        public Calc()
+        {
+            _semaphore = new Semaphore(3, 3);
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("start... " + Thread.CurrentThread.Name);
+
+            _semaphore.WaitOne();
+            Console.WriteLine("work start " + Thread.CurrentThread.Name);
+            Thread.Sleep(1000);
+            Console.WriteLine("work end " + Thread.CurrentThread.Name);
+            _semaphore.Release();
+
+            Console.WriteLine("end... " + Thread.CurrentThread.Name);
+        }
+    }
+}
---
+++ SemaphoreSample/SemaphoreSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calc();
+
+            var count = 10;
+
+            var threads = new List<Thread>();
+            for (var i=0;  i < count; i++)
+            {
+                var thread = new Thread(calc.Work);
+                thread.Name = i.ToString();
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
